7e2afaba62902928ed4c40b11c5621b5e50b482c
Erick Aytkulov <e.aytkulov@gmail.com>
2018-02-19 21:49:07 +0300

    divide tests by profiles,

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -47,9 +47,4 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return crudMealRepository.getBetween(startDate, endDate, userId);
     }
-
-    @Override
-    public Meal getWithUser (int id, int userId){
-        return crudMealRepository.getWithUser(id, userId);
-    }
 }
