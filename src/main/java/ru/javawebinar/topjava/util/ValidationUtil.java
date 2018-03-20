a8b356475c79b4b231c677451cf17eda863e63d4
Erick Aytkulov <e.aytkulov@gmail.com>
2018-03-20 22:29:58 +0300

    HW 9 MealRestController fix bugs

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.util;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
